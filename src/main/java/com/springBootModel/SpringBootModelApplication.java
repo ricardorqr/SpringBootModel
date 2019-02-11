db18924a5a150e647a8fa9285ee6b7a812167362
Ricardo Queiroz Ribeiro <ricardorqr@gmail.com>
2019-02-11 14:46:43 -0500

    Initial version

---
+++ src/main/java/com/springBootModel/SpringBootModelApplication.java
@@ -0,0 +1,14 @@
+package com.springBootModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModelApplication.class, args);
+	}
+
+}
+
